30c9a71b6624acfc1ba8611240576e561284116a
Kaan Keskindil <kkeskindil@mun.ca>
2026-04-06 23:14:27 -0230

    Add gitignore, remove build artifacts

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GUI_MAPS_FILE C:/Users/kaank/Downloads/reprojectupdate/build/Debug/GUI_MAPS.exe)
-set(__QT_DEPLOY_TARGET_GUI_MAPS_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GUI_MAPS_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Chartsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6OpenGLWidgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6OpenGLd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Cored.dll)
--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GUI_MAPS_FILE C:/Users/kaank/Downloads/reprojectupdate/build/MinSizeRel/GUI_MAPS.exe)
-set(__QT_DEPLOY_TARGET_GUI_MAPS_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GUI_MAPS_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Charts.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6OpenGL.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Core.dll)
